1be1819d01123b1d9ec622b6971c501d17d7d087
Toni & Elkana Lumbantobing <pair+chrisantoni.elkana-ltobing@hacktiv8.com>
2016-09-29 09:34:43 +0700

    public-vs-private

--- BankAccount.js
+++ BankAccount.js
@@ -4,26 +4,29 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    this.customer_name = customer_name
+    this.type = type
+    this._acct_number = acct_number
   }
-
+  set account_number(account_number) {
+    this._acct_number = account_number
+  }
   get account_number() {
-    //
+    return this._acct_number
   }
-
   to_s() {
-    //
+    console.log(this.customer_name + ":" + this.type + "#" + this.account_number);
   }
 
   cover_digits() {
-    //
+    console.log(this.account_number.replace(/\d+[-]\d+/, "***-***"))
   }
 }
 
 let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
-console.log(my_acct)
-console.log(my_acct.account_number)
+// console.log(my_acct)
+// console.log(my_acct.account_number)
 
 // release 0
 my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
