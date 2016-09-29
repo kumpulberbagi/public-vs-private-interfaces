8496daa3d529cada57f2b0c67d277b1a35aa739e
Mangku Widodo & YONI SETIAWAN <pair+mangkuwi.yonisetiawan@devbootcamp.com>
2016-09-29 10:40:02 +0700

    Bank Account done

--- BankAccount.js
+++ BankAccount.js
@@ -4,29 +4,41 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor(customer_name, type, acct_number) {
-    //
+    this._customer_name = customer_name;
+    this._type = type;
+    this._acct_number = acct_number;
+
+  }
+  set account_number(value){
+    this._acct_number = value;
   }
 
   get account_number() {
-    //
+    return this._acct_number;
   }
 
   to_s() {
-    //
+    var hasil = [];
+    hasil[this._customer_name]=this._type+"# "+this._acct_number
+    return hasil;
   }
 
   cover_digits() {
-    //
+    var hasil = [];
+    hasil[this._customer_name]=this._type+"# "+"***-***-"+this._acct_number.match(/\d{3}$/g)
+    return hasil;
   }
+
+
 }
 
 let my_acct = new BankAccount("Hacktivate", "Checking", "333-555-888")
 
 console.log(my_acct)
 console.log(my_acct.account_number)
-
+console.log("++++++++++++++++++")
 // release 0
-my_acct.to_s() // "Hacktivate: Checking# 333-555-888"
+console.log(my_acct.to_s()) // "Hacktivate: Checking# 333-555-888"
 
 // release 1
-my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
+console.log(my_acct.cover_digits()) // "Hacktivate: Checking# ***-***-888"
